dac56ebaebe8569920e243eca5a98c519aebae98
mehmet-mhy <mehmet.mhy@gmail.com>
2024-04-15 16:51:05 -0600

    updated package versions

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name="cha",
-    version="0.3.0",
+    version="0.3.1",
     packages=find_packages(),
     license="MIT",
     description="A simple CLI chat tool to easily interface with OpenAI's LLM models",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     install_requires=[
-        "openai==1.16.2",
+        "openai==1.18.0",
         "tiktoken==0.6.0",
         "beautifulsoup4==4.12.3",
         "selenium==4.19.0",
         "webdriver-manager==4.0.1",
-        "yt-dlp==2024.03.10",
+        "yt-dlp==2024.4.9",
         # NOTE: (3-13-2024) this package did not have a PyPi page, so it was forked and git was used instead of PyPi
         "climage @ git+https://github.com/MehmetMHY/CLImage.git#egg=climage",
     ],
