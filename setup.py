1b8c60b7b1a5eba8e6a3b2ec69bf2b61314940e7
mehmet-mhy <mehmet.mhy@gmail.com>
2024-07-14 20:14:07 +0300

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cha",
-    version="0.4.5",
+    version="0.4.6",
     packages=find_packages(),
     license="MIT",
     description="A simple CLI chat tool to easily interface with OpenAI's LLM models",
