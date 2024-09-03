73c69ec7f49d4be39d4d2496a28c33239eb19829
mehmet-mhy <mehmet.mhy@gmail.com>
2024-09-03 16:33:52 +0300

    moved grq and cla out of experimental/ to cha/ to make stuff easier

--- cha/experimental/config.py
+++
@@ -1,8 +0,0 @@
-INITIAL_PROMPT = (
-    "You are a helpful assistant who keeps your response short and to the point"
-)
-MULTI_LINE_SEND = "END"
-MULI_LINE_MODE_TEXT = "!m"
-CLEAR_HISTORY_TEXT = "!c"
-SAVE_CHAT_HISTORY = "!s"
-EXIT_STRING_KEY = "!e"
