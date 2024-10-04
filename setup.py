72c561bf247d278d676790ceb57d2e2e8408ba9c
mehmet-mhy <mehmet.mhy@gmail.com>
2024-10-04 14:21:42 +0300

    updated dependencies

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name="cha",
-    version="0.6.1",
+    version="0.6.2",
     packages=find_packages(),
     license="MIT",
     description="A simple CLI chat tool for easy interaction with OpenAI's LLM models and other platforms",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     install_requires=[
-        "openai==1.43.0",
+        "openai==1.51.0",
         "anthropic==0.34.2",
         "groq==0.11.0",
         "beautifulsoup4==4.12.3",
-        "selenium==4.24.0",
+        "selenium==4.25.0",
         "webdriver-manager==4.0.2",
-        "yt-dlp==2024.8.6",
-        "PyMuPDF==1.24.10",
+        "yt-dlp==2024.9.27",
+        "PyMuPDF==1.24.11",
         # NOTE: (3-13-2024) Using Git because PyPi page does not exist
         "climage @ git+https://github.com/MehmetMHY/CLImage.git#egg=climage",
     ],
