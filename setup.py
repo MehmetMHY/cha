4b2867810a0ec32d99ac8e956d981ebf22f33b37
mehmet-mhy <mehmet.mhy@gmail.com>
2025-02-28 20:29:43 -0500

    codedump - fixed bugs

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="cha",
-    version="0.15.3",
+    version="0.15.4",
     packages=find_packages(),
     license="MIT",
     description="A simple CLI tool for chatting, web scraping, and doing other CLI tasks with OpenAI's models",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     install_requires=[
-        "openai==1.65.0",
-        "anthropic==0.48.0",
+        "openai==1.65.1",
+        "anthropic==0.49.0",
         "beautifulsoup4==4.13.3",
         "yt-dlp==2025.2.19",
         "youtube-transcript-api==0.6.3",
