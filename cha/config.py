d2eb5db5d2fe5f683f9e77cc9bf81f7321b13ea2
mehmet-mhy <mehmet.mhy@gmail.com>
2025-03-24 23:31:01 -0700

    debugging

--- cha/config.py
+++ cha/config.py
@@ -245,6 +245,14 @@ THIRD_PARTY_PLATFORMS = {
         "env_name": "XAI_API_KEY",
         "docs": "https://docs.x.ai/docs/overview",
     },
+    "anthropic": {
+        "package_name": "cha.cla",
+        "function": "anthropic",
+        "env_name": "ANTHROPIC_API_KEY",
+        "parameters": {
+            "select_model": True,
+        },
+    },
 }
 
 # urls that contain video data that can be scrapped using cha's scraper
