4fa4c195f1b8c5755a02f0baa9eebe169991ea43
mehmet-mhy <mehmet.mhy@gmail.com>
2025-04-13 02:45:08 -0600

    refined docker logic

--- assets/docker/run.sh
+++ assets/docker/run.sh
@@ -4,32 +4,26 @@
 IMAGE_NAME="gotty-term"
 URL="http://localhost:8080/"
 
-# display help if -h parameter is passed
-if [[ "$1" == "-h" || "$2" == "-h" ]]; then
-	echo "Name: Cha Docker Setup Tool"
-	echo "Usage: run.sh [options]"
-	echo "Options:"
-	echo "  -b  Rebuild the Docker image"
-	echo "  -o  Open the application URL in the browser"
-	echo "  -h  Display this help message"
-	exit 0
-fi
+# ask user if they want to build the image
+read -p "Do you want to build the Docker image? (yes/y): " build_image
+build_image=$(echo "$build_image" | tr '[:upper:]' '[:lower:]')
 
-# build the image if it does not exist or ask to user to rebuild it
-if [[ "$(docker images -q ${IMAGE_NAME} 2>/dev/null)" == "" ]]; then
-	echo "Docker image ${IMAGE_NAME} does not exist. Building it now."
-	docker build --build-arg CACHE_DATE="$(date)" -t ${IMAGE_NAME} .
-else
-	if [[ "$1" == "-b" || "$2" == "-b" ]]; then
+# build the image if it does not exist or ask user to rebuild it
+if [[ "$(docker images -q ${IMAGE_NAME} 2>/dev/null)" == "" || "$build_image" == "yes" || "$build_image" == "y" ]]; then
+	if [[ "$(docker images -q ${IMAGE_NAME} 2>/dev/null)" != "" ]]; then
 		echo "Removing existing ${IMAGE_NAME} image..."
 		docker rmi -f ${IMAGE_NAME}
-		echo "Building new ${IMAGE_NAME} image..."
-		docker build --build-arg CACHE_DATE="$(date)" -t ${IMAGE_NAME} .
 	fi
+	echo "Building new ${IMAGE_NAME} image..."
+	docker build --build-arg CACHE_DATE="$(date)" -t ${IMAGE_NAME} .
 fi
 
-# open link to instance in the browser
-if [[ "$1" == "-o" || "$2" == "-o" ]]; then
+# ask user if they want to open the application URL in the browser
+read -p "Do you want to open the application URL in the browser? (yes/y): " open_url
+open_url=$(echo "$open_url" | tr '[:upper:]' '[:lower:]')
+
+# open link to instance in the browser if user agrees
+if [[ "$open_url" == "yes" || "$open_url" == "y" ]]; then
 	if [[ "$OSTYPE" == "darwin"* ]]; then
 		open $URL
 	elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
