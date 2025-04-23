ccb424f26bfd6c1166475f232ddf227f2aec104a
mehmet-mhy <mehmet.mhy@gmail.com>
2025-04-23 17:54:22 -0600

    refinements

--- assets/checkup.py
+++ assets/checkup.py
@@ -9,6 +9,24 @@ import os
 import re
 
 
+def passed(text):
+    text = f"✓ {text}"
+    print(f"\033[92m{text}\033[0m")
+    return
+
+
+def warning(text):
+    text = f"! {text}"
+    print(f"\033[93m{text}\033[0m")
+    return
+
+
+def failed(text):
+    text = f"✗ {text}"
+    print(f"\033[91m{text}\033[0m")
+    return
+
+
 def parse_setup_py(file_path="setup.py"):
     with open(file_path, "r") as file:
         content = file.read()
@@ -76,18 +94,18 @@ def checkup():
         response = requests.get("http://www.google.com", timeout=5)
         if response.status_code != 200:
             raise Exception("")
-        print(f"✓ You are connected to the internet!")
+        passed(f"You are connected to the internet!")
     except:
-        print("✗ Not connected to the internet; failing ALL tests")
+        failed("Not connected to the internet; failing ALL tests")
         return
 
     try:
         response = requests.get("https://duckduckgo.com", timeout=5)
         if response.status_code != 200:
             raise Exception("")
-        print(f"✓ DuckDuckGo search engine seems to be up and running")
+        passed(f"DuckDuckGo search engine seems to be up and running")
     except requests.exceptions.RequestException as e:
-        print("✗ DuckDuckGo search engine seems to be down")
+        failed("DuckDuckGo search engine seems to be down")
 
     try:
         PYTHON_SETUP_FILE_PATH = None
@@ -115,18 +133,18 @@ def checkup():
 
         result = check_installed_packages_with_pip(packages)
         if len(result["missing"]) > 0:
-            print(f"✗ Cha's dependencies are not installed: {response['missing']}")
+            failed(f"Cha's dependencies are not installed: {response['missing']}")
         else:
-            print("✓ All of Cha's dependencies are installed")
+            passed("All of Cha's dependencies are installed")
     except:
-        print("✗ Failed to check if Cha's dependencies are installed or not")
+        failed("Failed to check if Cha's dependencies are installed or not")
 
     try:
         import cha
 
-        print(f"✓ Cha is installed")
+        passed(f"Cha is installed")
     except:
-        print("✗ Cha is NOT installed, skipping all tests!")
+        failed("Cha is NOT installed, skipping all tests!")
         pass
 
     # check if required environment variables are loaded
@@ -138,14 +156,14 @@ def checkup():
             if env_name.lower().strip() == "ollama":
                 continue
             if env_name in os.environ:
-                print(f"✓ (OPTIONAL) {env_name} is set")
+                passed(f"{env_name} is set")
                 continue
-            print(f"! (OPTIONAL) {env_name} variable is missing")
+            warning(f"{env_name} variable is missing")
 
         if "OPENAI_API_KEY" in os.environ:
-            print("✓ OPENAI_API_KEY is set")
+            passed("OPENAI_API_KEY is set")
         else:
-            print("✗ OPENAI_API_KEY variable is missing")
+            failed("OPENAI_API_KEY variable is missing")
     except:
         pass
 
@@ -158,9 +176,9 @@ def checkup():
         url = config.THIRD_PARTY_PLATFORMS["ollama"]["models"]["url"]
         response = requests.get(url, timeout=3)
         if response.status_code == 200:
-            print("✓ Ollama is running locally")
+            passed("Ollama is running locally")
         else:
-            print("! Ollama is not running locally")
+            warning("Ollama is not running locally")
     except:
         pass
 
@@ -172,9 +190,9 @@ def checkup():
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
         )
-        print("✓ Tool ffmpeg seems to be installed")
+        passed("Tool ffmpeg seems to be installed")
     except (subprocess.CalledProcessError, FileNotFoundError):
-        print("✗ Tool ffmpeg is not installed, please install it: https://ffmpeg.org/")
+        failed("Tool ffmpeg is not installed, please install it: https://ffmpeg.org/")
 
     # check if whisper model is installed and working
     try:
@@ -186,9 +204,9 @@ def checkup():
         )
         del model
 
-        print(f"✓ Whisper model found")
+        passed(f"Whisper model found")
     except Exception as e:
-        print(f"✗ failed to find Whisper model {e}")
+        failed(f"failed to find Whisper model {e}")
 
     # check to make sure at least one of the supported Terminal IDEs are installed
     try:
@@ -196,14 +214,14 @@ def checkup():
 
         test_failed = True
         if any(shutil.which(editor) for editor in config.SUPPORTED_TERMINAL_IDES):
-            print("✓ Supported Terminal IDE is installed")
+            passed("Supported Terminal IDE is installed")
             test_failed = False
         if test_failed:
             raise Exception(
                 f"user's systems has none of the supported terminal text editor(s) installed"
             )
     except:
-        print(f"✗ None of the supported terminal IDE(s) are installed")
+        failed(f"None of the supported terminal IDE(s) are installed")
 
     # check if git is installed or not
     try:
@@ -213,21 +231,26 @@ def checkup():
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
         )
-        print("✓ Git found (for code-dump feature)")
+        passed("Git found (for code-dump feature)")
     except (subprocess.CalledProcessError, FileNotFoundError):
-        print("✗ Git not installed or not on PATH")
+        failed("Git not installed or not on PATH")
 
     # check if yt-dlp is installed, correctly, or not
     if shutil.which("yt-dlp"):
-        print("✓ yt-dlp found (for YouTube transcripts)")
+        passed("yt-dlp found (for YouTube transcripts)")
     else:
-        print("✗ yt-dlp not installed or not on PATH")
+        failed("yt-dlp not installed or not on PATH")
 
     valid_shell_count = num_of_valid_shells()
     if valid_shell_count > 0:
-        print("✓ One valid shell was found for Cha's shell mode")
+        passed("One valid shell was found for Cha's shell mode")
     else:
-        print("✗ Zero valid shells found for Cha's shell mode")
+        failed("Zero valid shells found for Cha's shell mode")
+
+    if os.path.isdir(os.path.join(str(Path.home()), ".cha/")):
+        passed("$HOME/.cha/ directly exists!")
+    else:
+        failed("$HOME/.cha/ directly does NOT exist!")
 
 
 if __name__ == "__main__":
