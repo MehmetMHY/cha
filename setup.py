1bf129d3b942ffe8016afd28275905e6bcdee4fb
mehmet-mhy <mehmet.mhy@gmail.com>
2025-06-30 18:22:54 -0600

    updated dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cha",
-    version="0.19.11",
+    version="0.19.12",
     packages=find_packages(),
     license="MIT",
     description="A simple cli tool that simplifies interactions with AI models, offering features like chat, web scraping, and answer search. It supports multiple platforms and file types, enhancing productivity for all.",
@@ -11,7 +11,7 @@ setup(
     install_requires=[
         "openai==1.93.0",
         "beautifulsoup4==4.13.4",
-        "yt-dlp==2025.6.25",
+        "yt-dlp==2025.6.30",
         "youtube-transcript-api==1.1.0",
         "youtube-comment-downloader==0.1.76",
         "PyMuPDF==1.26.1",
