511dea934247b4979bd37d100f6ba5fc8ecbdd44
mehmet-mhy <mehmet.mhy@gmail.com>
2025-07-21 03:27:26 -0600

    bump version to 0.22.2 and update ddgs dependency to 9.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cha",
-    version="0.22.1",
+    version="0.22.2",
     packages=find_packages(),
     license="MIT",
     description="A simple cli tool that simplifies interactions with AI models, offering features like chat, web scraping, and answer search. It supports multiple platforms and file types, enhancing productivity for all.",
@@ -16,7 +16,7 @@ setup(
         "youtube-comment-downloader==0.1.76",
         "PyMuPDF==1.26.3",
         "tiktoken==0.9.0",
-        "ddgs==9.3.1",
+        "ddgs==9.4.0",
         "python-docx==1.2.0",
         "openpyxl==3.1.5",
         "chardet==5.2.0",
